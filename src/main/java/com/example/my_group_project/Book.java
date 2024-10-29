177ede7867b8643cb576f186e43e7301e2107a0c
23021757yen <163371468+23021757yen@users.noreply.github.com>
2024-10-29 23:25:08 +0700

    commit

--- src/main/java/com/example/my_group_project/Book.java
+++ src/main/java/com/example/my_group_project/Book.java
@@ -1,11 +1,99 @@
 package com.example.my_group_project;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import java.util.List;
+
 class Book {
+    private Image imageSrc;
     private String title;
     private String authors;
+    private String content;
+    private int pageViews;
+    private int numberOfRead;
+    private int numberOfBorrow;
+    private List<String>category ;
+
+    public Book(Image imageSrc, String title, String authors, String content, int pageViews,
+                int numberOfRead, int numberOfBorrow, List<String> category) {
+        this.imageSrc = imageSrc;
+        this.title = title;
+        this.authors = authors;
+        this.content = content;
+        this.pageViews = pageViews;
+        this.numberOfRead = numberOfRead;
+        this.numberOfBorrow = numberOfBorrow;
+        this.category = category;
+
+    }
 
     public Book(String title, String authors) {
         this.title = title;
+        this.authors = authors;
+    }
+
+    public List<String> getCategory() {
+        return category;
+    }
+
+    public void setCategory(List<String> category) {
+        this.category = category;
+    }
+
+    public int getNumberOfBorrow() {
+        return numberOfBorrow;
+    }
+
+    public void setNumberOfBorrow(int numberOfBorrow) {
+        this.numberOfBorrow = numberOfBorrow;
+    }
+
+    public int getNumberOfRead() {
+        return numberOfRead;
+    }
+
+    public void setNumberOfRead(int numberOfRead) {
+        this.numberOfRead = numberOfRead;
+    }
+
+    public int getPageViews() {
+        return pageViews;
+    }
+
+    public void setPageViews(int pageViews) {
+        this.pageViews = pageViews;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
         this.authors = authors;
     }
 
@@ -13,7 +101,15 @@ class Book {
         return title;
     }
 
-    public String getAuthors() {
-        return authors;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Image getImageSrc() {
+        return imageSrc;
+    }
+
+    public void setImageSrc(Image imageSrc) {
+        this.imageSrc = imageSrc;
     }
 }
--- src/main/java/com/example/my_group_project/HelloApplication.java
+++ src/main/java/com/example/my_group_project/HelloApplication.java
@@ -5,12 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcomeToWebsite.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("bookProfile.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
